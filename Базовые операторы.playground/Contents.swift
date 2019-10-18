0555b9b0a67f72ccf7908bc8cfb7735e8c08cce0
amirhanov <rustam.amirhanov@gmail.com>
2019-10-18 18:35:54 +0300

    Commit

--- Базовые операторы.playground/Contents.swift
+++ Базовые операторы.playground/Contents.swift
@@ -1,27 +1,27 @@
 import UIKit
 
-//MARK:- Assignment operator
+//MARK:- Оператор присваивания
 
 let a = "Hello"
 
-//MARK:- Arithmetic operators
+//MARK:- Арифметические операторы
 
 let b = 1 + 2
 let c = "Hello" + " ,World"
 let d = 6 / 2
 
-//MARK:- Integer division operator
+//MARK:- Оператор целочисленного деления
 
 let e = 100 % 6
 
-//MARK:- Compound assignment operators
+//MARK:- Cоставные операторы присваивания
 
 let f = 1
 var g = 0
 //g = g + f
 g += f
 
-//MARK:- Comparison operators
+//MARK:- Операторы сравнения
 
 let h = 10
 let i = 20
@@ -32,20 +32,20 @@ h != i
 h <= i
 h == i
 
-//MARK:- Nil replacing operator
+//MARK:- Оператор объединения по nil
 
 let placeholder = "Steve"
 var name: String? = "Anna"
 let personName = name ?? placeholder
 
-//MARK:- Unary and ternary operators
+//MARK:- Унарный и тернарный оператор
 
 -1
 
 let j = true
 let k = j ? 1 : 2
 
-//MARK:- Logical operators
+//MARK:- Логические операторы
 
 let day = 6
 let mood = true
@@ -57,7 +57,7 @@ if day >= 6 && mood == true {
 let l = true
 let m = !l
 
-//MARK:- Range operators
+//MARK:- Операторы диапазона
 
 1...10
 1..<10
